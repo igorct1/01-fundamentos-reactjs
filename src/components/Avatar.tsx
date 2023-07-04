0d781dcf780169a6a598b9b6adb2888af13d24e6
igoreason <igorct77@gmail.com>
2023-07-04 18:40:45 -0300

    fix: fix avatar interface

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,17 @@
 import styles from './Avatar.module.css';
+import { ImgHTMLAttributes } from 'react';
 
-interface AvatarProps {
-	src: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
 	hasBorder?: boolean;
 }
 
-export function Avatar({ src, hasBorder = true }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
 	return (
 		<img
 			className={`${
 				hasBorder ? styles.avatar : styles.avatarWithoutBorder
 			}`}
-			src={src}
+			{...props}
 		/>
 	);
 }
